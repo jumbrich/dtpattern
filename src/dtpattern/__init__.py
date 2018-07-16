78fd4fa6ad5151d8cabce8c7c4fad8cda04a55a8
jumbrich <jueumb@gmail.com>
2018-07-16 16:09:05 +0200

    bumped version

--- src/dtpattern/__init__.py
+++ src/dtpattern/__init__.py
@@ -10,7 +10,7 @@ try:
     dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
-    __version__ = '0.6'
+    __version__ = '0.7'
 __author__ = """Juergen Umbrich"""
 __email__ = 'jueumb@gmail.com'
 
